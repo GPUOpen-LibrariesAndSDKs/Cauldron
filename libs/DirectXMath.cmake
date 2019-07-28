2157e268317c3c4da0f2b4bf96ed00376aee9a45
Mikhail Paulyshka <me@mixaill.tk>
2019-07-29 02:06:31 +0300

    libs/DirectXMath: improved GCC support
    get rid of SAL.h

--- libs/DirectXMath.cmake
+++ libs/DirectXMath.cmake
@@ -1,6 +1,2 @@
 add_library(DirectXMath INTERFACE)
 target_include_directories(DirectXMath INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/libs/DirectXMath/Inc")
-
-if(NOT WIN32)
-    target_include_directories(DirectXMath INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/libs/sal")
-endif()
